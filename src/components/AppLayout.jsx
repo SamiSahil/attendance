12e6d65901e8901bf181635bd90863e7a0539f4c
SamiSahil <samisahil498@gmail.com>
2025-09-17 10:38:27 +0600

    1

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,15 +1,16 @@
 import { NavLink } from 'react-router-dom';
 import './AppLayout.css';
 
+// The `children` prop will be the page component we want to display (e.g., <StudentList />)
 const AppLayout = ({ children }) => (
   <div>
-    <nav className="navbar">
-           <NavLink to="/">Student List</NavLink>
+    <nav className="navbar"> 
+      <NavLink to="/">Student List</NavLink>
       <NavLink to="/attendance">Mark Attendance</NavLink>
       <NavLink to="/report">Attendance Report</NavLink>
     </nav>
     <main className="content">
-     
+      {/* Instead of an <Outlet />, we now directly render the child page */}
       {children}
     </main>
   </div>
